c9bb6b913d43af7ab2f2d9c4534a7dd35f865584
matheus fagnani <fagnanimatheusarara20008@gmail.com>
2024-05-14 14:25:05 -0300

    criando tela

--- furti_CHICO BENTO.py
+++ furti_CHICO BENTO.py
@@ -1,1 +1,18 @@
-__py
+import pygame
+pygame.init()
+#Constrindo a tela
+tela = pygame.display.set_mode((800,500))
+pygame.display.set_caption("Freeway")
+tela.fill((80,120,200))
+
+clock = pygame.time.Clock()
+
+rodando = True
+while rodando:
+    #Tratando eventos
+    for evento in pygame.event.get():
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            print("Você clicou!!")
+        if evento.type == pygame.QUIT:
+            rodando = False
+            
